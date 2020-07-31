edd03585dd55a3a4c39e8928a120bbd9dd59eb5b
wangt316 <wangt316@myumanitoba.ca>
2020-07-31 16:00:44 -0500

    prototype complete

--- ClientSide/Communication/Client.py
+++ ClientSide/Communication/Client.py
@@ -3,31 +3,36 @@
 #message receiving  counter= MAX(message_receving_counter,counter)++
 from Communication.LamportCounter import *
 import socket
+import pickle
 class communication_client:
     def __init__(self):
         self.lamport= lamportTime()
         self.port=10086
-        self.socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.Socket.connect(('LOCALHOST', self.port))
+        self.pos= pickle.loads(self.Socket.recv(2048))
+    def send(self,string):
 
-    def send(self,str):
-        try:
-            self.socket.connect(('', self.port))
-            str+="&Counter="+self.lamport.getCounter()
-            self.socket.sendall(str.encode())
-            self.lamport.incrementCounter()
-            print("Sent:"+str)
-            self.socket.close()
-        except:
-            print("Failed to send message")
+        string+="&Counter="+str(self.lamport.getCounter())
+        self.Socket.sendall(string.encode())
+        self.lamport.incrementCounter()
+        print("Sent:"+string)
+
         return
     def receive(self):
-        sendMessage="GET"
-        self.socket.connect(('', self.port))
-        self.socket.sendall(sendMessage.encode())
-        reve_counter=self.socket.recv(2048).decode()[2]
-        counter=int(reve_counter.split("=")[1])
-        maxValue= max(self.lamport.getCounter(),counter)
-        self.lamport.setCounter(maxValue)
-        self.lamport.incrementCounter()
-        return reve_counter
+        try:
+            sendMessage="GET"
+            self.Socket.sendall(sendMessage.encode())
+
+            data=self.Socket.recv(2048).decode("utf-8")
+            counter=int(data.split("&")[2].split("=")[1])
+            maxValue= max(self.lamport.getCounter(),counter)
+            self.lamport.setCounter(maxValue)
+            self.lamport.incrementCounter()
+            return data
+        except socket.error as e:
+            print(e)
+            return ""
+    def getPos(self):
+        return str(self.pos)
 
--- serverside/server.py
+++ serverside/server.py
@@ -1,8 +1,8 @@
 from _thread import *
 import socket
 import sys
-
-host = ""
+import pickle
+host = "LOCALHOST"
 port=10086
 playerInfo = {}
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,8 +13,8 @@ print("Server is started, waitting for hearing from clients:")
 def initialList(list):
     list['player1'] = {}
     list['player2'] = {}
-    list['player1']['posX'] = 0
-    list['player1']['posY'] = 0
+    list['player1']['posX'] = 10
+    list['player1']['posY'] = 10
     list['player1']['Counter'] = 0
     list['player2']['posX'] = 500
     list['player2']['posY'] = 500
@@ -23,25 +23,26 @@ def make_pos(player,list):
     #the position need to send to opposite player
     strings = ""
     if player == 0:
-        strings = "posX="+str(playerInfo['player1']['posX'])+"&posY="+str(playerInfo['player1']['posY'])+"&Counter="+str(playerInfo['player1']['Counter'])
+        strings = "posX="+str(list['player1']['posX'])+"&posY="+str(list['player1']['posY'])+"&Counter="+str(list['player1']['Counter'])
     elif player == 1:
-        strings = "posX="+str(playerInfo['player2']['posX'])+"&posY="+str(playerInfo['player2']['posY'])+"&Counter="+str(playerInfo['player2']['Counter'])
+        strings = "posX="+str(list['player2']['posX'])+"&posY="+str(list['player2']['posY'])+"&Counter="+str(list['player2']['Counter'])
     return strings
-def read_pos(player,list,data):
+def read_pos(player,data):
     #this is update player's position on server
     if player == 0:
         for info in data.split('&'):
             (key,value) = info.split('=')
-            list['player1'][key]=value
+            playerInfo['player1'][key]=value
     elif player == 1:
         for info in data.split('&'):
             (key,value) = info.split('=')
-            list['player2'][key]=value
+            playerInfo['player2'][key]=value
 
+    return
 def threaded_client(conn,player):
     #First of first, we need to send initial position to current player
     #conn.sendall(str.encdoe("Player"+str(player)+"is connected"))
-    conn.send(str.encode(make_pos(player,playerInfo)))
+    conn.send(pickle.dumps(make_pos(player,playerInfo)))
     reply = ""
     while True:
         try:
@@ -53,18 +54,18 @@ def threaded_client(conn,player):
                     oppositePos = make_pos(1, playerInfo)
                 elif player == 1:
                     oppositePos = make_pos(0, playerInfo)
-                print(oppositePos)
                 conn.sendall(str.encode(oppositePos))
             else:
                 # Server do not need to respond any information, just update current player's position
-                read_pos(player, playerInfo, data)
+                read_pos(player, data)
+                print(data)
         except:
             break
     player -=1
     conn.close()
     print("Disconnected")
 initialList(playerInfo)
-print(len(playerInfo))
+print(playerInfo)
 while True:
     conn, addr=serversocket.accept()
     print("Now connected to this address", addr)
