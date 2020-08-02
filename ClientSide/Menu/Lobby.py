b381d8324942c305b5f3f4ed274c0fdcadf7ed51
CBM <chenboma@hotmail.com>
2020-08-01 19:22:10 -0500

    Lobby
    Add cap button, continue working

--- ClientSide/Menu/Lobby.py
+++ ClientSide/Menu/Lobby.py
@@ -9,9 +9,11 @@ red= (255,0,0)
 class lobby:
     def __init__(self):
         self.ready=False
-        self.init=False
+        self.cap = False
+        self.thief = False
+        self.init =False
     def draw(self, win,font):
-        pygame.draw.line(win, black, (0, resolution_height / 5), (resolution_width, resolution_width / 5), 10)
+        pygame.draw.line(win, black, (0, resolution_height / 5), (resolution_width, resolution_height / 5), 10)
         pygame.draw.line(win, black, (resolution_width / 2, resolution_height / 5),
                          (resolution_width / 2, resolution_height / 5 * 4), 10)
         pygame.draw.line(win, black, (0, resolution_height / 5 * 4), (resolution_width, resolution_width / 5 * 4), 10)
@@ -22,22 +24,43 @@ class lobby:
             text = font.render('Ready', True, black, green)
         else:
             text = font.render('Cancel', True, black, red)
-        rect = text.get_rect()
-        rect.center = (resolution_width // 2, resolution_height * 6 / 7)
+        if not self.cap:
+            textcap = font.render('Police', True, black, green)
+        else:
+            textcap = font.render('Police', True, black, red)
+        readyB = text.get_rect()
+        readyB.center = (resolution_width / 2, resolution_height * 6 / 7)
+        capB = text.get_rect()
+        capB.center = (resolution_width / 4, resolution_height/10)
+        print("This is capB.center"+str(capB.center))
         if not self.init:
-            self.rect_left=rect.left
-            self.rect_right = rect.right
-            self.rect_bottom=rect.bottom
-            self.rect_top=rect.top
+            self.readyB_left=readyB.left
+            self.readyB_right = readyB.right
+            self.readyB_bottom=readyB.bottom
+            self.readyB_top=readyB.top
+            self.capB_left = capB.left
+            self.capB_right = capB.right
+            self.capB_bottom = capB.bottom
+            self.capB_top = capB.top
             self.init=True
-        win.blit(text, rect)
+        win.blit(text, readyB)
+        win.blit(textcap,capB)
+        print("This is readyB"+str(readyB))
+        print("This is capB"+str(capB))
         return
     def changeReady(self):
         if self.init:
             (x, y) = pygame.mouse.get_pos()
-            if (x >= self.rect_left and x <= self.rect_right and y >= self.rect_top and y <= self.rect_bottom):
+            if (x >= self.readyB_left and x <= self.readyB_right and y >= self.readyB_top and y <= self.readyB_bottom):
                 if (not self.ready):
                     self.ready = True
                 else:
                     self.ready = False
+            if (x >= self.capB_left and x <= self.capB_right and y >= self.capB_top and y <= self.capB_bottom):
+                if (not self.cap):
+                    self.cap = True
+                else:
+                    self.cap = False
+        return
+    def chooseC(self):
         return
