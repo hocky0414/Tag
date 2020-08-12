2335c694b95e86153775d1a5c41c8da7add7b527
wangt316 <wangt316@myumanitoba.ca>
2020-08-12 15:35:09 -0500

    Fixed some bugs

--- serverside/server.py
+++ serverside/server.py
@@ -8,6 +8,8 @@ import time
 import pickle
 
 print("Server is started, waitting for hearing from clients:")
+
+
 def initialList(list):
     list['cap'] = {}
     list['thief'] = {}
@@ -16,66 +18,72 @@ def initialList(list):
     list['cap']['Counter'] = 0
     list['thief']['posX'] = 500
     list['thief']['posY'] = 500
-    list['thief']['Counter']=0
+    list['thief']['Counter'] = 0
     return
 
+
 def make_pos(player):
-    #the position need to send to opposite player
+    # the position need to send to opposite player
 
     if player == 0:
-       return players[0]
+        return players[0]
     elif player == 1:
         return players[1]
 
-def threaded_client(conn,player):
+
+def threaded_client(conn):
     recordTime = True
-    gamelength = 60
+    gamelength = 10
     caught = False
+    role = -1
     startTime = time.time()
     start = False
     ends = False
     temp = {}
     status = Status.gameStatus()
     while not caught:
-        #try:
+        try:
             data = pickle.loads(conn.recv(2048))
-            if type(data)==type(player_thief):
+            if type(data) == type(player_thief):
                 # The server need to respond opposite player's opposite player's position
-                players[player] = data
-                oppositePos = None
-                if player == 0:
-                    oppositePos = make_pos(1)
-                elif player == 1:
-                    oppositePos = make_pos(0)
-                conn.sendall(pickle.dumps(oppositePos))
-            elif type(data)==type(""):
+                if role != -1:
+                    players[role] = data
+                    oppositePos = None
+                    if role == 0:
+                        oppositePos = make_pos(1)
+                    elif role == 1:
+                        oppositePos = make_pos(0)
+                    conn.sendall(pickle.dumps(oppositePos))
+                else:
+                    print("Not in game yet")
+            elif type(data) == type(""):
                 if "status" in data:
-                    print(data)
                     conn.sendall(pickle.dumps(status.getStates()))
                 elif "&" in data:
                     for ele in data.split("&"):
-                        (k,v) = ele.split("=")
-                        temp[k]=v
+                        (k, v) = ele.split("=")
+                        temp[k] = v
                         print(v)
                     if "thief" in temp['character']:
-                        role=0
+                        role = 0
                     else:
-                        role=1
-                    if "True" in temp['ready'] and not readyPlayer[role] :
-                        readyPlayer[role]=True
+                        role = 1
+                    if "True" in temp['ready'] and not readyPlayer[role]:
+                        readyPlayer[role] = True
                         conn.sendall(pickle.dumps("ok"))
                     elif "True" in temp['ready'] and readyPlayer[role]:
                         conn.sendall(pickle.dumps("taken"))
                     elif "False" in temp['ready']:
-                        readyPlayer[role]=False
+                        readyPlayer[role] = False
                         conn.sendall(pickle.dumps("ok"))
                 elif "init" in data:
                     if "thief" in temp['character']:
-                        #print(make_pos(1))
                         conn.sendall(pickle.dumps(make_pos(0)))
+                        print("Theif:" + str(make_pos(0).getPosition()))
+
                     elif 'cap' in temp['character']:
-                       # print(make_pos(0))
                         conn.sendall(pickle.dumps(make_pos(1)))
+                        print("Police:" + str(make_pos(1).getPosition()))
             if False in readyPlayer:
                 start = False
             else:
@@ -83,56 +91,55 @@ def threaded_client(conn,player):
             if start and (not ends):
                 status.setIngame()
                 if recordTime:
-                    startTime = time.time()#record start time
+                    startTime = time.time()  # record start time
                     recordTime = False
             currentTime = time.time()
             policePos = players[1].getPosition()
             thiefPos = players[0].getPosition()
-            if (currentTime-startTime)<gamelength and start and not ends:
-                if(abs(policePos[0]-thiefPos[0])<PlayerStatic.getWid())and(abs(policePos[1]-thiefPos[1])<PlayerStatic.getHeight()): #caught
+            if (currentTime - startTime) < gamelength and start and not ends:
+                if (abs(policePos[0] - thiefPos[0]) < PlayerStatic.getWid()) and (
+                        abs(policePos[1] - thiefPos[1]) < PlayerStatic.getHeight()):  # caught
                     ends = True
-                    if player == 0:
+                    if role == 0:
                         status.setLose()
-                    if player == 1:
+                    if role == 1:
                         status.setWin()
-            elif (currentTime-startTime)>=gamelength and start and not ends:
+            elif (currentTime - startTime) >= gamelength and start and not ends:
                 print("Out of time")
                 ends = True
-                if player == 0:
-                    print(player)
+                if role == 0:
                     print("Thief wins")
                     status.setWin()
-                if player == 1:
-                    print(player)
+                if role == 1:
                     print("Police lose")
                     status.setLose()
-        # except:
-        #     break
-    player -=1
+        except:
+            break
     conn.close()
     print("Disconnected")
+
+
 host = ''
-port=10086
+port = 10086
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind((host,port))
-serversocket.listen(2)
-maxPlayer=2
+serversocket.bind((host, port))
+serversocket.listen(20)
+maxPlayer = 2
 currentPlayer = 0
 
-readyPlayer= [False,False]
+readyPlayer = [False, False]
 playerInfo = {}
 initialList(playerInfo)
 map = Map.Map(1000, 1000)
-player_thief= player.PlayerObj(int(playerInfo['thief']['posX']), (playerInfo['thief']['posY']), PlayerStatic.getWid(), PlayerStatic.getHeight(), (255, 0, 0), map.block(), map)
-player_police= player.PlayerObj(int(playerInfo['cap']['posX']),(playerInfo['cap']['posY']),PlayerStatic.getWid(),PlayerStatic.getHeight(),(0,255,0),map.block(),map)
-players=[player_thief,player_police]
-
+player_thief = player.PlayerObj(10, 10, PlayerStatic.getWid(), PlayerStatic.getHeight(), (255, 0, 0), map.block(), map)
+player_police = player.PlayerObj(500, 500, PlayerStatic.getWid(), PlayerStatic.getHeight(), (0, 255, 0), map.block(),
+                                 map)
+players = [player_thief, player_police]
 while True:
-    conn, addr=serversocket.accept()
+    conn, addr = serversocket.accept()
     print("Now connected to this address", addr)
-    start_new_thread(threaded_client,(conn,currentPlayer))
+    start_new_thread(threaded_client, (conn,))
 
-    print("This is player"+str(currentPlayer))
-    currentPlayer +=1
-
+    print("This is player" + str(currentPlayer))
+    currentPlayer += 1
